64cd60b8403c3cb045b8e9529eeecb10139c2548
Xingmin Zhang <kingmanzhang@gmail.com>
2021-05-09 16:44:40 -0400

    implement get LabHpo for a TextHpo

--- src/main/python/mimic_mf_analysis/cluster_analysis.py
+++ src/main/python/mimic_mf_analysis/cluster_analysis.py
@@ -3,6 +3,9 @@ import community as community_louvain
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import csv
+from obonetx.ontology import Ontology
+import re
+import pandas as pd
 
 
 def load_mutual_information_pairs(path: str):
@@ -10,25 +13,37 @@ def load_mutual_information_pairs(path: str):
     with open(path, 'r') as f:
         reader = csv.DictReader(f)
         for line in reader:
-            mutual_information_pairs.append((line['P1'], line['P2'], float(line['mf'])))
+            mutual_information_pairs.append(('P1_' + line['P1'], 'P2_' + line['P2'], float(line['mf'])))
     return mutual_information_pairs
 
 
 if __name__=='__main__':
+    hpo = Ontology('/Users/Aaron/git/human-phenotype-ontology/hp.obo')
+    hpo_term_map = hpo.term_id_2_label_map()
+
     # The following analysis is more suited to be in a Notebook in ad hoc analysis
-    path = '/Users/Aaron/Desktop/mf_labHpo_labHpo - mf_labHpo_labHpo.csv'
+    path = '/Users/Aaron/Desktop/mf_textHpo_labHpo - mf_textHpo_labHpo.csv'
     mf_paris = load_mutual_information_pairs(path)
-    mf_pairs_filters = [x for x in mf_paris if x[2] >= 0.20]
+    mf_pairs_filters = [x for x in mf_paris if x[2] >= 0.07]
     G = nx.Graph()
     G.add_weighted_edges_from(mf_pairs_filters)
 
     partition = community_louvain.best_partition(G)
-    print(partition)
 
     pos = nx.spring_layout(G)
     cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-    labels = {x : x for x in partition.keys()}
-    nx.draw_networkx(G, pos, nodelist=partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()), labels=labels, font_size=7)
+    labels = {}
+    labels_textHpo = {x : 'textHpo: ' + hpo_term_map.get(re.sub('P1_', '', x), x) for x in partition.keys() if x.startswith('P1_')}
+    labels_labHpo = {x: 'labHpo: ' + hpo_term_map.get(re.sub('P2_', '', x), x) for x in partition.keys() if
+                     x.startswith('P2_')}
+    labels.update(labels_textHpo)
+    labels.update(labels_labHpo)
+    print(pd.DataFrame(data={'node': partition.keys(), 'partition': partition.values()}))
+    # print([1 if x.startswith('textHpo') else 2 for x in labels.values()])
+    # labels = {x : hpo_term_map.get(re.sub('P[12]_', '', x), x) for x in partition.keys()}
+    # nx.draw_networkx(G, pos, nodelist=partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()), labels=labels, font_size=7)
+    nx.draw_networkx(G, pos, nodelist=partition.keys(), node_size=40, cmap=cmap, node_color=[1 if x.startswith('P1') else 3 for x in partition.keys()],
+                     labels=labels, font_size=7)
 
     plt.show()
 
---
+++ src/main/python/mimic_mf_analysis/predict_textHpo.py
@@ -0,0 +1,64 @@
+from mimic_mf_analysis import mydb
+import pandas as pd
+
+
+def get_labHpo_for_textHpo(textHpo, leadtime=0):
+    """
+    Given a phenotype recorded by a textHpo, find labHpo terms observed before the specified lead time.
+    Algorithm:
+    1. identify which patient has been observed with the query textHpo, and record the time of observation
+    2. query the labHpo and find all record before the date when the textHpo is observed.
+    :param textHpo: a phenotype observed from texts
+    :param leadtime: if not None, find lab-derived phenotypes with a leadtime before the text-derived phenotype
+    :return:
+    """
+    with mydb.cursor() as cursor:
+        cursor.execute(f'''
+            CREATE TEMPORARY TABLE labHpoForTextHpo
+            WITH textHpo_of_interest AS (
+            SELECT
+                NOTEEVENTS.SUBJECT_ID, NOTEEVENTS.HADM_ID, NoteHpoClinPhen.MAP_TO, NOTEEVENTS.CHARTTIME
+            FROM 
+                NOTEEVENTS 
+            JOIN NoteHpoClinPhen on NOTEEVENTS.ROW_ID = NoteHpoClinPhen.NOTES_ROW_ID
+            WHERE NoteHpoClinPhen.MAP_TO = '{textHpo}'
+            UNION ALL
+            SELECT
+                NOTEEVENTS.SUBJECT_ID, NOTEEVENTS.HADM_ID, Inferred_NoteHpo.INFERRED_TO AS MAP_TO, NOTEEVENTS.CHARTTIME
+            FROM 
+                NOTEEVENTS 
+            JOIN Inferred_NoteHpo on NOTEEVENTS.ROW_ID = Inferred_NoteHpo.NOTEEVENT_ROW_ID
+            WHERE Inferred_NoteHpo.INFERRED_TO = '{textHpo}'
+            ),
+            patientOfInterest AS (
+                SELECT SUBJECT_ID, MIN(CHARTTIME) as earliest_observation_date
+                FROM textHpo_of_interest
+                GROUP BY SUBJECT_ID),
+            raw AS (
+                select
+                    LABEVENTS.SUBJECT_ID, LABEVENTS.HADM_ID, LabHpo.MAP_TO, LABEVENTS.CHARTTIME
+                FROM LabHpo
+                JOIN LABEVENTS on LABEVENTS.ROW_ID = LabHpo.ROW_ID
+                JOIN patientOfInterest on LABEVENTS.SUBJECT_ID = patientOfInterest.SUBJECT_ID
+                WHERE LabHpo.NEGATED = 'F')
+            SELECT 
+                LABEVENTS.SUBJECT_ID, LABEVENTS.HADM_ID, INFERRED_LABHPO.INFERRED_TO AS MAP_TO, LABEVENTS.CHARTTIME
+            FROM 
+                INFERRED_LABHPO 
+            JOIN 
+                LABEVENTS ON INFERRED_LABHPO.LABEVENT_ROW_ID = LABEVENTS.ROW_ID
+            JOIN patientOfInterest on LABEVENTS.SUBJECT_ID = patientOfInterest.SUBJECT_ID 
+            UNION ALL
+            SELECT *
+            FROM raw
+        ''')
+
+        labHpo = pd.read_sql('''
+            SELECT * FROM labHpoForTextHpo;
+        ''', mydb)
+        print(labHpo.head())
+
+
+if __name__=='__main__':
+    # Thromboembolism
+    get_labHpo_for_textHpo(textHpo='HP:0001907', leadtime=0)
